b023e4134c7a87c3ee00c8ee9ffd248f85974927
aasmundo <aasmundo@stud.ntnu.no>
2014-09-29 23:48:34 +0200

    test commit

--- rs232 oving5/menu.c
+++ rs232 oving5/menu.c
@@ -3,7 +3,7 @@
  *
  * Created: 15.09.2014 17:18:59
  *  Author: gunnalid
- */ 
+ */
 #include "menu.h"
 
 
@@ -11,17 +11,17 @@ void create_menu(){
 	struct menu *main_menu;
 	*main_menu->title = "Main menu";
 	main_menu->num_submenus = 3;
-	
-	
+
+
 	*main_menu->submenu[0]->title = "Aasmund er homo!";
 	*main_menu->submenu[1]->title = "Gunnar er homo!";
 	*main_menu->submenu[2]->title = "Jon er homo!";
-	
+
 	oled_address_reset();
 	printf(main_menu->title + '\n');
 	printf(main_menu->submenu[0]->title + '\n');
 	printf(main_menu->submenu[1]->title + '\n');
 	printf(main_menu->submenu[2]->title + '\n');
-	
+	printf("dette er bare en test");
 }
 
